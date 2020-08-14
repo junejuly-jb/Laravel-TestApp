66498a3a913fdb2f73b5f5fd199e8a21b8d1be3c
june <junearagoncillo>
2020-08-14 11:36:38 +0800

    first commit

---
+++ resources/views/addons/nav.blade.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">{{config('app.name', 'Laravel App')}}</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link" href="/index">Home</a>
+            <a class="nav-link" href="#">About</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Pricing</a>
+          </div>
+        </div>
+    </nav>
+</div>
